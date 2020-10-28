382ae11565c7612d8eebf79c8c6040e682a13473
tito arandia <fabian.arandia@gmail.com>
2020-10-27 21:07:48 -0400

    changement de la date à la main.

---
+++ main.py
@@ -0,0 +1,13 @@
+import xarray as xr
+import fsspec 
+import s3fs
+client_kwargs={'region_name':'us-east-1'}
+config_kwargs = {'max_pool_connections': 30}
+
+fs = s3fs.S3FileSystem(client_kwargs=client_kwargs, 
+                       config_kwargs=config_kwargs, 
+                       anon=True)  # public read
+
+fs.ls('noaa-gfs-bdp-pds/gfs.20201027/00/')
+
+fs.download('noaa-gfs-bdp-pds/gfs.20201027/00/gfs.t00z.pgrb2full.0p50.f027','gfs.t00z.pgrb2full.0p50.f027.grib2')
